569e78da586a9614617e9a913f341b9623820840
bolcsgondolat <32794042+bolcsgondolat@users.noreply.github.com>
2017-10-14 13:44:52 +0200

    I have modified my dad program.
    fixing the torch placement (if we not got any torch we should place anything)

---
+++ mining/Nutty.lua
@@ -0,0 +1,46 @@
+
+
+local util = dofile("util.lua")
+util.hello()
+
+function GoDown(times)
+	local bedrock = false
+	
+	for i=1, times do
+		turtle.dig()
+		turtle.forward()
+		
+		see, block = turtle.inspect()
+		if see and block.name == "minecraft:bedrock" then
+			bedrock = true
+			
+			print("Elertem az aljat :)")
+			-- break
+			
+			turtle.up()
+		end
+		
+		turtle.digUp()
+		turtle.digDown()
+		
+		if i % 8 == 0 then
+			PlaceTorch()
+		end
+		
+		if not bedrock then
+			turtle.down()
+		end
+	end
+end
+
+function PlaceTorch()
+	util.turn()
+	
+	if util.select("minecraft:torch") then 
+		turtle.place()
+	end
+	
+	util.turn()
+end
+
+GoDown(100)
