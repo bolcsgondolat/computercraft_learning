bca904e951f096bc2eb563fc1b346419ddb2bc79
bolcsgondolat <32794042+bolcsgondolat@users.noreply.github.com>
2017-10-14 13:46:09 +0200

    Changing my dad program.
    The select has the return, true if found the target.

---
+++ mining/util.lua
@@ -0,0 +1,28 @@
+local util = {}
+
+function util.hello()
+	print("Hello from util :)")
+end
+
+function util.select(name)
+	for i=1, 16 do
+		info = turtle.getItemDetail(i)
+		
+		if info then
+			if info.name == name then
+				turtle.select(i)
+
+				return true
+			end
+		end
+	end
+
+	return false
+end
+
+function util.turn()
+	turtle.turnLeft()
+	turtle.turnLeft()
+end
+
+return util
